06829d144adf6d4b19bff7fce643b73357296a4a
rahqan.shaikh <rahqan.shaikh@HP2304.asl.mum.sst>
2025-07-30 20:38:14 +0530

    commit 1 ffs

--- AssignmentUniversityAdmin/src/com/aurionpro/UniversityAdmin/UniversityAdmin.java
+++ AssignmentUniversityAdmin/src/com/aurionpro/UniversityAdmin/UniversityAdmin.java
@@ -5,7 +5,7 @@ public class UniversityAdmin {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("HEllo world!");
+		System.out.println("hello world! by rahqan");
 
 	}
 
