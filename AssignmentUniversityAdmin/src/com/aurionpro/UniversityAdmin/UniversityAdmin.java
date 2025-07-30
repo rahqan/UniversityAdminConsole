95742868a2bd1e3815195d3c52dc779701ae9a0f
rahqan <107210281+rahqan@users.noreply.github.com>
2025-07-30 19:05:28 +0530

    Add files via upload

---
+++ AssignmentUniversityAdmin/src/com/aurionpro/UniversityAdmin/UniversityAdmin.java
@@ -0,0 +1,12 @@
+package com.aurionpro.UniversityAdmin;
+
+public class UniversityAdmin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("HEllo world!");
+
+	}
+
+}
---
+++ AssignmentUniversityAdmin/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module AssignmentUniversityAdmin {
+}
