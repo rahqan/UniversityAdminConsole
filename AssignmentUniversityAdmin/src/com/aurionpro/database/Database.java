9cb281c6da53ad12d43a6efbb5e538056b8c68bf
unknown <arjun.pareek>
2025-07-31 18:32:56 +0530

    Database implementation

--- AssignmentUniversityAdmin/src/com/aurionpro/database/Database.java
+++ AssignmentUniversityAdmin/src/com/aurionpro/database/Database.java
@@ -1,5 +1,32 @@
 package com.aurionpro.database;
 
 public class Database {
+	private static final Database INSTANCE = new Database();
 
+	Connection connection = null;
+
+	public Database() {
+		if (INSTANCE != null) {
+			throw new RuntimeException("use getInstance() method");
+		}
+	}
+
+	public static getInstance() {
+		return INSTANCE;
+	}
+
+	public void connect() {
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/university", "root", "admin#123");
+			System.out.println("Connection to database has been established");
+		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+
+	}
 }
