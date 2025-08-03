a7e15a574ea3d8a82cf4f7ac7652975dcfa2205e
atharva.ambike <atharva.ambike@HP2328.asl.mum.sst>
2025-08-03 16:24:17 +0530

    Added database connection code

--- AssignmentUniversityAdmin/src/com/aurionpro/database/Database.java
+++ AssignmentUniversityAdmin/src/com/aurionpro/database/Database.java
@@ -1,5 +1,34 @@
 package com.aurionpro.database;
 
-public class Database {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class Database {
+    private static Database instance;
+    private Connection connection;
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/University";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "root";
+
+    private Database() throws SQLException {
+        try {
+            Class.forName("org.postgresql.Driver");
+            this.connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("PostgreSQL JDBC Driver not found.", e);
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public static Database getInstance() throws SQLException {
+        if (instance == null || instance.getConnection().isClosed()) {
+            instance = new Database();
+        }
+        return instance;
+    }
 }
