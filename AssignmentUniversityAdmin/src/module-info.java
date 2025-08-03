c06e5365749cce610471f5a80261a1aa2a14243f
atharva.ambike <atharva.ambike@HP2328.asl.mum.sst>
2025-08-03 16:24:56 +0530

    Edited module-info.java

--- AssignmentUniversityAdmin/src/module-info.java
+++ AssignmentUniversityAdmin/src/module-info.java
@@ -5,4 +5,10 @@
  * 
  */
 module AssignmentUniversityAdmin {
+    requires java.sql;
+    opens com.aurionpro.model;
+    opens com.aurionpro.database;
+    opens com.aurionpro.dao;
+    opens com.aurionpro.service;
+    opens com.aurionpro.controller;
 }
