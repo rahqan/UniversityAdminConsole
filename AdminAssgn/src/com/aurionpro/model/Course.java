988030db7fe45fdccd5959486991aa68a03fb5d4
Chingapo <115269983+Chingapo@users.noreply.github.com>
2025-08-05 21:29:18 +0530

    Update Course.java

--- AdminAssgn/src/com/aurionpro/model/Course.java
+++ AdminAssgn/src/com/aurionpro/model/Course.java
@@ -5,10 +5,12 @@ import java.util.List;
 public class Course {
 	private int courseId;
 	private String name;
+	private Double courseFee;
 
-	public Course(int courseId, String name) {
+	public Course(int courseId, String name, Double courseFee) {
 		this.courseId = courseId;
 		this.name = name;
+		this.courseFee = courseFee;
 	}
 
 	public int getCourseId() {
@@ -18,4 +20,22 @@ public class Course {
 	public String getName() {
 		return name;
 	}
+
+	public Double getCourseFee() {
+		return courseFee;
+	}
+
+	public void setCourseFee(Double courseFee) {
+		this.courseFee = courseFee;
+	}
+
+	public void setCourseId(int courseId) {
+		this.courseId = courseId;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
